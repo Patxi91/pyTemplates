8fb6426c411f223b49611cdec52cc6b578d5a4ca
Patxi <francisco.oyaga-landa@akka.eu>
2020-12-11 23:17:34 +0100

    Digital Image with Python.

--- Digital_Image.py
+++ Digital_Image.py
@@ -1,6 +1,31 @@
+# Pillow python Imaging Library adds image processing capabilities to your Python interpreter.
+from PIL import Image
+
+img = Image.open(r'C:\Users\Patxi\Downloads\images\images\profile.jpg')
+img.show()
+
+new_img = img.convert('L')  # Convert image to grayscale
+new_img.show()
+
+
+# Analyze Image with numpy and matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
+from pylab import *
 
 A = plt.imread(r'C:\Users\Patxi\Downloads\images\images\profile.jpg')
 plt.imshow(A)
 plt.show()
+print(A)
+print(np.shape(A))
+print(type(A))
+print(A.dtype)
 
+im = array(Image.open(r'C:\Users\Patxi\Downloads\images\images\profile.jpg'))
+imshow(im)
+x = [100,150,300,400]
+y = [50, 500, 200, 500]
+plot(x,y,'r*')
+plot(x[:2],y[:2],'r')
+plot(x[2:],y[2:],'ks:')
+show()
