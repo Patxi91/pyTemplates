466fded5a9e366ff2d3f5aace8e85f4f8356db27
Patxi91 <oyaga888@gmail.com>
2020-12-29 20:19:44 +0100

    Anagram Check

--- iq_anagram_check.py
+++ iq_anagram_check.py
@@ -1,7 +1,10 @@
+from nose.tools import assert_equals, assert_equal
+
 # Interview question
 '''
 Anagram Check:
-Given two strings, check to see if they are anagrams. An anagram is when the two strings can be written using the exact same letters (so you can just rearrange the letters to get a different phrase or word).
+Given two strings, check to see if they are anagrams. An anagram is when the two strings can be written using the exact
+same letters (so you can just rearrange the letters to get a different phrase or word).
 
 For example:
 "public relations" is an anagram of "crap built on lies."
@@ -9,3 +12,29 @@ For example:
 
 Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g".
 '''
+
+
+def anagram(s1, s2):
+    s1_aux = s1.replace(" ", "").lower()
+    s2_aux = s2.replace(" ", "").lower()
+    if sorted(s1_aux) == sorted(s2_aux):
+        sol = True
+    else:
+        sol = False
+    return sol
+
+
+class AnagramTest(object):
+
+    def test(self, sol):
+        assert_equal(sol('go go go', 'gggooo'), True)
+        assert_equal(sol('abc', 'cba'), True)
+        assert_equal(sol('hi man', 'hi     man'), True)
+        assert_equal(sol('aabbcc', 'aabbc'), False)
+        assert_equal(sol('123', '1 2'), False)
+        print("ALL TEST CASES PASSED")
+
+
+# Run Tests
+t = AnagramTest()
+t.test(anagram)
