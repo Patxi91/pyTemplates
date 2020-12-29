42cebe1cd12978cb7602fe6d81707ed4f9243120
Patxi91 <oyaga888@gmail.com>
2020-12-29 15:12:42 +0100

    Array sequences in Python

--- Arrray_sequences.py
+++ Arrray_sequences.py
@@ -1,4 +1,5 @@
 import sys
+import ctypes
 
 # Array Sequences
 # List: [1,2,3], Tuple: (1,2,3), String: '123'
@@ -6,7 +7,7 @@ import sys
 arr = [0]*5  # arr = [0,0,0,0,0] where each index points to the same memory address.
 print(id(arr[0]) == id(arr[1]))
 
-# Dynamic Arrays
+# Dynamic arrays memory allocation
 n = 10
 data = []
 for i in range(n):
@@ -15,7 +16,60 @@ for i in range(n):
     print(f'Length: {a}, Size: {b}')
     data.append(n)  # Increase Size by one
 
+
 # Dynamic array implementation
+    '''
+    DYNAMIC ARRAY CLASS (Similar to Python List)
+    '''
+
+class DynamicArray(object):
+    def __init__(self):
+        self.n = 0
+        self.capacity = 1
+        self.A = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, k):
+        if not 0 <= k < self.n:
+            return IndexError('k is out of bounds')
+        return self.A[k]
+
+    def append(self, ele):
+        """
+        Add element to end of the array
+        """
+        if self.n == self.capacity:
+            self._resize(2*self.capacity)  # 2x if capacity isn't enough
+        self.A[self.n] = ele
+        self.n += 1
+
+    def _resize(self, new_cap):
+        """
+        Resize internal array to capacity new_cap
+        """
+        B = self.make_array(new_cap)
+        for k in range(self.n):
+            B[k] = self.A[k]
+        self.A = B
+        self.capacity = new_cap
+
+    def make_array(self, new_cap):
+        """
+        Returns a new array with new_cap capacity
+        """
+        return (new_cap * ctypes.py_object)()
+
+arr = DynamicArray()
+n = 10
+for i in range(n):
+    a = len(arr)  # Number of Data
+    b = sys.getsizeof(arr.A)  # Actual Size in Bytes
+    print(f'Dynamic array Length: {a}, Size: {b}')
+    arr.append(n)
+
+
 
 
 
