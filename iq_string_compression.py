d72123af9cdd8f646dd4a95fefcd3daaf4c4c66c
Patxi91 <oyaga888@gmail.com>
2020-12-30 17:59:05 +0100

    String Compression

---
+++ iq_string_compression.py
@@ -0,0 +1,61 @@
+from nose.tools import assert_equal
+
+# Interview question
+'''
+String Compression:
+Given a string in the form 'AAAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2E4'.
+It is okay for 'AAB' to return 'A2B1' even though this technically takes more space.
+The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
+'''
+
+
+def compress(s):
+    # Begin Run as empty string
+    r = ""
+    l = len(s)
+
+    # Check for length 0
+    if l == 0:
+        return ""
+
+    # Check for length 1
+    if l == 1:
+        return s + "1"
+
+    # Initialize Values
+    last = s[0]
+    cnt = 1
+    i = 1
+
+    while i < l:
+
+        # Check to see if it is the same letter
+        if s[i] == s[i - 1]:
+            # Add a count if same as previous
+            cnt += 1
+        else:
+            # Otherwise store the previous data
+            r = r + s[i - 1] + str(cnt)
+            cnt = 1
+
+        # Add to index count to terminate while loop
+        i += 1
+
+    # Put everything back into run
+    r = r + s[i - 1] + str(cnt)
+
+    return r
+
+
+class Test:
+
+    def test(self, sol):
+        assert_equal(sol(''), '')
+        assert_equal(sol('AABBCC'), 'A2B2C2')
+        assert_equal(sol('AAABCCDDDDD'), 'A3B1C2D5')
+        print('ALL TEST CASES PASSED')
+
+
+# Run and test
+t = Test()
+t.test(compress)  # Time and Space complexity of O(n).
