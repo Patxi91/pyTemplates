631cf79d442d5bcdf14be0d14841c98b2b82ed59
Patxi91 <oyaga888@gmail.com>
2020-12-30 14:26:48 +0100

    TestPair adjustment + PEP8

--- iq_largest_continuous_sum.py
+++ iq_largest_continuous_sum.py
@@ -7,14 +7,24 @@ Given an array of integers (positive and negative) find the largest continuous s
 '''
 
 
-def large_cont_sum(k):
-    return k
+def large_cont_sum(arr):
+
+    if len(arr) == 0:
+        return 0
+
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(current_sum+num, num)
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
 
 
 class TestPair:
 
     def test(self, sol):
-        assert_equal(sol(5), 5)
+        assert_equal(sol([1, 2, -1, 3, 4, 10, 10, -10, -1]), 29)
         print('ALL TEST CASES PASSED')
 
 
