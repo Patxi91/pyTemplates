ca7ddf8ed7b0fde4d5e25e7cfa05b399758a602b
Patxi91 <oyaga888@gmail.com>
2021-01-07 20:26:48 +0100

    Recursion - Coin change

--- unbenannt3.py
+++ unbenannt3.py
@@ -2,13 +2,6 @@ from nose.tools import assert_equal
 import time
 
 
-
-'''
-Fibonacci sequence: 0,1,1,2,3,5,8,13,21,... starts with a base case checking to see if n = 0 or 1, then it returns 1.
-Else it returns fib(n-1)+fib(n+2).
-'''
-
-
 # Dynamic solution Coins
 def coin_dyn(n, coins):
     
@@ -35,7 +28,18 @@ cache = {}
 n = 74
 coins = [1, 5, 10, 25]
 print(coin_dyn(n, coins))  # 8
-        
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
