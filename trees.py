3dcf929b27581b04de09c858cf31fdb882ad3e10
Patxi91 <oyaga888@gmail.com>
2021-01-08 12:25:57 +0100

    Trees

--- trees.py
+++ trees.py
@@ -1,5 +1,61 @@
 
 
+# Tree representation through Lists
+def BinaryTree(r):
+    return [r, [], []]
+
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)  # pop left tree
+
+    if len(t) > 1:
+        root.insert(1, [newBranch, t, []])
+    else:
+        root.insert(1, [newBranch, [], []])
+
+    return root
+
+
+def insertRight(root, newBranch):
+    t = root.pop(2)  # pop left tree
+
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+
+    return root
+
+
+def getRootVal(root):
+    return root[0]
+
+
+def setRootVal(root, new_val):
+    root[0] = new_val
+
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+
+r = BinaryTree(3)
+print(insertLeft(r, 4))  # [3, [4, [], []], []]
+print(insertLeft(r, 5))  # [3, [5, [4, [], []], []], []]
+print(insertRight(r, 6))  # [3, [5, [4, [], []], []], [6, [], []]]
+print(insertRight(r, 7))  # [3, [5, [4, [], []], []], [7, [], [6, [], []]]]
+
+l = getLeftChild(r)
+print(l)  # [5, [4, [], []], []]
+setRootVal(l, 9)
+print(l)  # [9, [4, [], []], []]
+
+print(r)  # [3, [9, [4, [], []], []], [7, [], [6, [], []]]]
+
+
 
 
 
