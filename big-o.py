92c32ad36bf195a3ea5209cf2303c0ab7d699411
Patxi91 <oyaga888@gmail.com>
2021-01-23 11:23:25 +0100

    Searching and Sorting

--- big-o.py
+++ big-o.py
@@ -1,6 +1,6 @@
 # Big-O: https://www.bigocheatsheet.com/
 # https://stackoverflow.com/questions/487258/what-is-a-plain-english-explanation-of-big-o-notation/487278#487278
-# https://stackoverflow.com/questions/487258/what-is-a-plain-english-explanation-of-big-o-notation/487278#487278
+
 
 # O(1) Constant
 def func_constant(values):
