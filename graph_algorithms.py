cbee92b87dbbdac166fee711b32a6b6854485314
Patxi91 <oyaga888@gmail.com>
2021-02-02 19:10:15 +0100

    Graph Algorithms

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -5,7 +5,7 @@ class Vertex:
         self.id = key
         self.connectedTo = {}
 
-    def add_neighbor(self, nbr, weight=0):
+    def add_neighbor(self, nbr, weight=0):  # called by Graph().add_edge
         self.connectedTo[nbr] = weight
 
     def get_connections(self):
@@ -24,13 +24,30 @@ class Vertex:
 class Graph:
 
     def __init__(self):
-        self.vertList = {}
+        self.vertList = {}  # Adjacency List dictionary
         self.numVertices = 0
 
     def add_vertex(self, key):
         self.numVertices += 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
+        return newVertex
+
+    def get_vertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def add_edge(self, fv, tv, weight=0):
+        if fv not in self.vertList:
+            nv = self.add_vertex(fv)
+        if tv not in self.vertList:
+            nv = self.add_vertex(tv)
+        self.vertList[fv].add_neighbor(tv, weight)  # calls Vertex().add_neighbor
+
+
+
 
 
 
