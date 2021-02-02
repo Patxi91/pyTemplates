953732510d277e58af0f65fd7862bf1e470c1a64
Patxi91 <oyaga888@gmail.com>
2021-02-02 18:06:13 +0100

    Graph Algorithms

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -1,5 +1,24 @@
-# Graphs: Set of vertices and set of edges, G=(V,E). Where V={V0,..., Vn} and E={(V0,V1,x),..., (Vn,Vn-i,y)}
+# Graph: Set of vertices and set of edges with weights. G=(V,E), where V={V0,..., Vn} and E={(V0,V1,x),..., (Vn,Vn-i,y)}
+class Vertex:
 
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def get_connections(self):
+        return self.connectedTo.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, nbr):
+        return self.connectedTo[nbr]
+
+    def __str__(self):
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
 
 
 
