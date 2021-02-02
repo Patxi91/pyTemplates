4c82861c2e28e3d6e7b644dd84139343df55df3a
Patxi91 <oyaga888@gmail.com>
2021-02-02 21:17:06 +0100

    Graph Algorithms

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -44,14 +44,28 @@ class Graph:
             nv = self.add_vertex(fv)
         if tv not in self.vertList:
             nv = self.add_vertex(tv)
-        self.vertList[fv].add_neighbor(tv, weight)  # calls Vertex().add_neighbor
+        self.vertList[fv].add_neighbor(self.vertList[tv], weight)  # calls Vertex().add_neighbor
+
+    def get_vertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+
+    def __contains__(self, item):
+        return item in self.vertList
 
 
+# Driver
 
+g = Graph()
 
+for i in range(6):
+    g.add_vertex(i)
 
+print(g.vertList)  # {0: <__main__.Vertex object at 0x04047928>, 1: <__main__.Vertex object at 0x04047520>, 2: <__main__.Vertex object at 0x040479A0>, 3: <__main__.Vertex object at 0x04047AC0>, 4: <__main__.Vertex object at 0x04047AD8>, 5: <__main__.Vertex object at 0x04047988>}
 
-
-
-
-
+g.add_edge(0, 1, 2)
+for vertex in g:  # Uses special method __iter__(self)
+    print(vertex)  # 0 connected to: [1]
+    print(vertex.get_connections())  # dict_keys([<__main__.Vertex object at 0x036CD328>])
