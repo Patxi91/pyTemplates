ac6ae620a0a98ba827c97b4bb51e0b874861d605
Patxi91 <oyaga888@gmail.com>
2021-02-13 19:48:46 +0100

    Word Ladder Graphs

--- graphs_iq_word_ladder.py
+++ graphs_iq_word_ladder.py
@@ -29,11 +29,9 @@ def buildGraph(wordFile):
     wfile = open(wordFile, 'r')
     # create buckets of words that differ by one letter
     for line in wfile:
-        print
-        line
+        print(line)
         word = line[:-1]
-        print
-        word
+        print(word)
         for i in range(len(word)):
             bucket = word[:i] + '_' + word[i + 1:]
             if bucket in d:
