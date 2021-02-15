b4dde871871504569b2873f0392c133e40dde08f
Patxi91 <oyaga888@gmail.com>
2021-02-15 08:49:40 +0100

    graphs_breadth_first_search_algorithm

---
+++ graphs_breadth_first_search_algorithm.py
@@ -0,0 +1,11 @@
+# Breadth First Search (BFS) see MIT lecture: https://www.youtube.com/watch?v=s-CYnVz-uh4
+'''
+Applications:
+- Web crawling
+- Social Network behaviour
+- Garbage collectors: Python, Cpython, Java, etc...
+- Model checking HW/SW --> Vertices are all states your program could reach
+-
+'''
+
+
