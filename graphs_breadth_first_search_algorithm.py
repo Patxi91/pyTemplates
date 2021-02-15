566b5024639ec1e8b9f22668e457a2e35e71cf55
Patxi91 <oyaga888@gmail.com>
2021-02-15 12:05:24 +0100

    graphs_breadth_first_search_algorithm

--- graphs_breadth_first_search_algorithm.py
+++ graphs_breadth_first_search_algorithm.py
@@ -5,7 +5,17 @@ Applications:
 - Social Network behaviour
 - Garbage collectors: Python, Cpython, Java, etc...
 - Model checking HW/SW --> Vertices are all states your program could reach
--
+- Checking mathematical conjecture --> Vertices are all possible inputs to theorem
+- Solving puzzles: 2x2 or 3x3 Rubik's Cube optimally
+
+Note: BFS is like building a tree one level at a time. The parents pointers give the shortest path.
+
+Keep track of progress: BFS colors each of the vertices white, gray or black.
+All vertices are initialized to white when they are constructed, a white vertex is an undiscovered vertex.
+The BFS begins at the starting vertex 's' and color gray (currently explored). For 's', distance=0 and predecessor=None.
+Start 's' is placed in a queue, next step is begin to explore vertices by iterating over its adjacency list.
+When a vertex is initially discovered it is colored gray.
+When BFS has completely explored a vertex, it is colored black.
 '''
 
 
