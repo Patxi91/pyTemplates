759b45bfcaeb85c5384b271ee1fd5c956c0f273a
Patxi91 <oyaga888@gmail.com>
2021-02-16 20:05:50 +0100

    graphs_general_depth_first_search

--- graphs_general_depth_first_search.py
+++ graphs_general_depth_first_search.py
@@ -1,2 +1,44 @@
+from graphs_algorithms import Graph
+
+'''
+General Depth First Search (DFS):
+Search as deep as possible, connecting as many nodes in the graph as possible, branching when necessary.
+
+The predecessor links (parents pointers) are used to construct the tree.
+:param: Discovery time: Tracks the number of steps in the algorithm before a vertex is first encountered,
+:param: Finish time: Number of steps in the algorithm before a vertex is colored black
+'''
+
+
+class DFSGraph(Graph):
+
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(-1)
+        for aVertex in self:
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex)
+
+    def dfsvisit(self, startVertex):
+        startVertex.setColor('gray')
+        self.time += 1
+        startVertex.setDiscovery(self.time)
+        for nextVertex in startVertex.getConnections():
+            if nextVertex.getColor() == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex)
+        startVertex.setColor('black')
+        self.time += 1
+        startVertex.setFinish(self.time)
+
+
+
+
+
 
 
