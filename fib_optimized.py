c18165f960465dcb524e8a40f4ddd072b9afb4c3
Patxi91 <oyaga888@gmail.com>
2021-03-23 01:07:41 +0100

    fib_optimized

--- fib_optimized.py
+++ fib_optimized.py
@@ -1,4 +1,4 @@
-
+import time
 
 # Dynamically -> Instantiate Cache information
 
@@ -33,6 +33,39 @@ f = memoize(f)
 result = fib(-1000) # -43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
 
 
+
+
+
+
+
+
+
+
+
+
+# Dynamically -> Instantiate Cache information
+
+cache = {}
+
+
+def fib_dyn(n):
+
+    # Base Case
+    if n == 0 or n == 1:
+        return n
+
+    # Check cache
+    if str(n) in cache:
+        return cache[str(n)]
+    else:  # Keep setting cache
+        cache[str(n)] = fib_dyn(n - 1) + fib_dyn(n - 2)
+
+    return cache[str(n)]
+
+
+for n in range(0, 2000, 50000):
+    print(fib_dyn(n))
+    time.sleep(0.1)  # Seconds
 
 
 
