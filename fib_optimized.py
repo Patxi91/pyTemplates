3760a20dddb1af03f8acef41b06ceaac5327b5be
Patxi91 <oyaga888@gmail.com>
2021-03-24 01:28:11 +0100

    fib_optimized

--- fib_optimized.py
+++ fib_optimized.py
@@ -1,57 +1,9 @@
-import sys
-sys.setrecursionlimit(50000)
-import time
-
-# Dynamically -> Instantiate Cache information
-
-def memoize(f):
-    memo = {}
-
-    def helper(x):
-        if x not in memo:
-            memo[x] = f(x)
-        return memo[x]
-
-    return helper
-
-
-def f(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return f(n - 1) + f(n - 2)
-
-
-def fib(n):
-    if n < 0 and n % 2 == 0:
-        return -f(abs(n))
-    else:
-        return f(n)
-
-
-f = memoize(f)
-result = fib(-1000) # -43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
-
-
-
-
-
-
-
-
-
-
-
-
-# Dynamically -> Instantiate Cache information
+# Dynamically -> Instantiate Cache information --> Execution Timed Out (12000 ms)
 
 cache = {}
 
 
 def fib_dyn(n):
-
     # Base Case
     if n == 0 or n == 1:
         return n
@@ -65,10 +17,38 @@ def fib_dyn(n):
     return cache[n]
 
 
-target = 500000 # nth Fibonacci
+def fib_cache(target):
+    step = 900
+    if target < 0 and target % 2 == 0:
+        for n in range(0, abs(target), step):
+            fib_dyn(n)
+        return -fib_dyn(abs(target))
+    elif target < 0:
+        for n in range(0, abs(target), step):
+            fib_dyn(n)
+        return fib_dyn(abs(target))
+    else:
+        for n in range(0, target, step):
+            fib_dyn(n)
+        return fib_dyn(target)
 
-for n in range(0, target, 2000):
-    fib_dyn(n)
-    print(n)
-    time.sleep(0.01)  # Seconds
-result = fib_dyn(target)
+
+# Mathematical Approach
+
+def fib(n):
+    if n >= 0:
+        return fibiter(1, 0, 0, 1, n)
+    if n < 0:
+        a, b = 0, 1
+        for _ in range(0, n, -1):
+            a, b = b - a, a
+        return a
+
+
+def fibiter(a, b, p, q, count):
+    if count == 0:
+        return b
+    if count % 2 == 0:
+        return fibiter(a, b, p * p + q * q, q * q + 2 * p * q, count / 2)
+    else:
+        return fibiter(b * q + a * q + a * p, b * p + a * q, p, q, count - 1)
