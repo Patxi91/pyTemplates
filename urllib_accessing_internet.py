1a7b095fa5b0f824c16062816eda147436cbe8bc
Patxi Oyaga <oyaga888@gmail.com>
2021-11-01 21:35:06 +0100

    urllib - decoding xml from webpage 1-->Sending data get 2-->Sending data post

--- urllib_accessing_internet.py
+++ urllib_accessing_internet.py
@@ -1,6 +1,7 @@
 # using urllib to request data
 
 import urllib.request
+import urllib.parse
 
 
 def main1():
@@ -21,5 +22,37 @@ def main1():
     print("Returned data: ------------------------")
     print(result.read().decode('utf-8'))
 
+
+def main2():
+    # the URL to retrieve our sample data from
+    url = "http://httpbin.org/get"
+
+    # create some data to pass to the GET request
+    args = {
+        "name": "Patxi Oyaga",
+        "is_author": True
+    }
+
+    # the data needs to be url-encoded before passing as arguments
+    data = urllib.parse.urlencode(args)
+
+    # issue the request with the data parameters as part of the URL
+    result1 = urllib.request.urlopen(url + "?" + data)  # data goes in args
+
+    print(f"Result code: {result1.status}")
+    print("Returned data: ------------------------")
+    print(result1.read().decode('utf-8'))
+
+    # issue the request with a data parameter to use POST
+    url = "http://httpbin.org/post"
+    data = data.encode()
+    result2 = urllib.request.urlopen(url, data=data)  # data goes in form (POST)
+
+    print(f"Result code: {result2.status}")
+    print("Returned data: ------------------------")
+    print(result2.read().decode('utf-8'))
+
+
 # Drivers
 main1()
+main2()
